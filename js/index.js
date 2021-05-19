78015a264834e9ae50f10897fa2b096ea493b749
Ionela Melania Stanciu <stanciuionelamelania@gmail.com>
2021-05-19 16:08:12 +0300

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,34 @@
+
+function home() {
+       location.reload();
+}
+
+function projects() {
+        $('#text-content').load("projects.html");
+        window.scrollTo(0, 0);
+}
+
+function team() {
+        $('#text-content').load("team.html");
+        window.scrollTo(0, 0);
+}
+
+function feedback() {
+        $('#text-content').load("feedback.html");
+        window.scrollTo(0, 0);
+}
+
+function contact() {
+        $('#text-content').load("contact.html");
+        window.scrollTo(0, 0);
+}
+
+function whyUs() {
+        $('#text-content').load("why_us.html");
+        window.scrollTo(0, 0);
+}
+
+function sendCv() {
+        $('#text-content').load("send_cv.html");
+        window.scrollTo(0, 0);
+}
